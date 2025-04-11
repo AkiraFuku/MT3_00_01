242dc0d947ec3c0b409fc2852a1e42a05800a426
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-04-11 16:25:54 +0900

    プロジェクト ファイルを追加します。

---
+++ MassFunction.cpp
@@ -0,0 +1,66 @@
+﻿#include "MassFunction.h"
+//namespace KamataEngine{
+	void VectorScreenPrintf(int x, int y, Vector3& vector, const char* label){
+		Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+		Novice::ScreenPrintf(x + KColumnWidth, y, "%.02f", vector.y);
+		Novice::ScreenPrintf(x + KColumnWidth * 2, y, "%.02f", vector.z);
+		Novice::ScreenPrintf(x + KColumnWidth * 3, y, "%s", label);
+	}
+	//加算
+	Vector3 Add(const Vector3& v1, const Vector3& v2){
+		Vector3 result={
+			v1.x + v2.x,
+			v1.y + v2.y,
+			v1.z + v2.z
+		
+		};
+		return result;
+	}
+	//
+	Vector3 Subtract(const Vector3& v1, const Vector3& v2){
+		Vector3 result = {
+			v1.x - v2.x,
+			v1.y - v2.y,
+			v1.z - v2.z
+		};
+		return result;
+	}
+	//
+	Vector3 Multiply(float scalar, const Vector3& v){
+		Vector3 result = {
+			scalar * v.x,
+			scalar * v.y,
+			scalar * v.z
+		};
+		return result;
+	}
+	//
+	float Dot(const Vector3& v1, const Vector3& v2){
+		
+		float result = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+		return result ;
+	}
+	//
+	float Length(const Vector3& v){
+		float length = v.x * v.x + v.y * v.y + v.z * v.z;
+		if (length > 0.0f) {
+			return sqrtf(length);
+		}
+		return length;
+	}
+	//
+	Vector3 Normalize(const Vector3& v){
+		float length = Length(v);
+		if (length > 0.0f) {
+			Vector3 result = {
+				v.x / length,
+				v.y / length,
+				v.z / length
+			};
+			return result;
+		}
+		Vector3 result = { 0.0f, 0.0f, 0.0f };
+		return result;
+
+	}
+//}
---
+++ MassFunction.h
@@ -0,0 +1,63 @@
+﻿#pragma once
+//#include <KamataEngine.h>
+#include <Novice.h>
+#include <math.h>
+struct Vector3{
+	float x;	///< x座標
+	float y;	///< y座標
+	float z;	///< z座標
+	
+};
+///
+	static const int KColumnWidth = 60;
+	static const int KRowHeight = 15;
+	/// <summary>
+	/// ベクトルの値の描画
+	/// </summary>
+	/// <param name="x">描画位置ｘ</param>
+	/// <param name="y">描画位置ｙ</param>
+	/// <param name="vector">ベクトル</param>
+	/// <param name="label">関数名</param>
+	void VectorScreenPrintf( int x, int y,Vector3& vector, const char* label);
+
+	/// <summary>
+	/// ADD 
+	/// </summary>
+	/// <param name="v1">ベクトル１</param>
+	/// <param name="v2">ベクトル２</param>
+	/// <returns>ベクトル１＋２</returns>
+	Vector3 Add(const Vector3& v1,const Vector3& v2);
+	/// <summary>
+	/// Subtract
+	/// </summary>
+	/// <param name="v1">ベクトル１</param>
+	/// <param name="v2">ベクトル２</param>
+	/// <returns>ベクトル１－２</returns>
+	Vector3 Subtract(const Vector3& v1,const Vector3& v2);
+	/// <summary>
+	/// スカラー倍
+	/// </summary>
+	/// <param name="scalar">スカラー数</param>
+	/// <param name="v">ベクトル</param>
+	/// <returns>ベクトル＊スカラー</returns>
+	Vector3 Multiply(float scalar,const Vector3& v);
+	/// <summary>
+	/// DOT
+	/// </summary>
+	/// <param name="v1">ベクトル１</param>
+	/// <param name="v2">ベクトル２</param>
+	/// <returns></returns>
+	float Dot(const Vector3& v1,const Vector3& v2);
+	/// <summary>
+	/// Length
+	/// </summary>
+	/// <param name="v">ベクトル</param>
+	/// <returns></returns>
+	float Length(const Vector3& v);
+	/// <summary>
+	/// Normalize
+	/// </summary>
+	/// <param name="v">ベクトル</param>
+	/// <returns>正規化数</returns>
+	Vector3 Normalize(const Vector3& v);
+//}
